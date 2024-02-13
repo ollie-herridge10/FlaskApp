2bceb86f252e3f34e15c5ff91440c8a3d7c91c68
Ollie Herridge <515238@waes.ac.uk>
2024-02-13 15:47:52 +0000

    added some html pages and some bootstrap

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard/index.html')
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('dashboard/about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
